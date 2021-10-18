266f8a4428c58c53ba23629b2c9dd33fe0d0d016
wblueDean <jaepo.park@gmail.com>
2021-10-18 14:54:03 -0400

    sql

--- SQL_schema.sql
+++ SQL_schema.sql
@@ -1,32 +1,32 @@
 CREATE TABLE Users
-(uid : CHAR(10),
-name : VARCHAR(20),
-present_mood : INTEGER,
-email : VARCHAR(35),
-is_active : BOOL,
+(uid CHAR(10),
+name VARCHAR(20),
+present_mood INTEGER,
+email VARCHAR(35),
+is_active BOOL,
 PRIMARY KEY(uid),
 UNIQUE(email))
 
 CREATE TABLE Groups(
-(group_id : CHAR(10),
- type : INTEGER,
- group_name : VARCHAR(50)
+(group_id CHAR(10),
+ type INTEGER,
+ group_name VARCHAR(50)
  PRIMARY KEY(group_id),
  UNIQUE(group_name))
 
 CREATE TABLE Dep_posts              --weak entity relationship
-(post_no : INTEGER,
- time : DATETIME,
- uid : CHAR(10),
+(post_no INTEGER,
+ time DATETIME,
+ uid CHAR(10),
  PRIMARY KEY(post_num, uid),
  FOREIGN KEY (uid) REFERENCES Users)
 
 CREATE TABLE Group_posts
-(uid : CHAR(10),
- group_id : CHAR(10),   --NOT NULL
- post_no : INTEGER,
- text : VARCHAR(200),
- image_URL : VARCHAR(100),
+(uid CHAR(10),
+ group_id CHAR(10) NOT NULL,
+ post_no INTEGER,
+ text VARCHAR(200),
+ image_URL VARCHAR(100),
  PRIMARY KEY(uid, post_no),
  FOREIGN KEY(uid, post_no) REFERENCES Dep_posts
  	ON DELETE CASCADE,
@@ -36,11 +36,11 @@ CREATE TABLE Group_posts
 --longitude and latitude represents location of the user when posting 
 
 CREATE TABLE Personal_mood
-(longitude : DECIMAL,
- latitude : DECIMAL, 
- uid : CHAR(10),
- post_no : INTEGER,
- image_URL : VARCHAR(100)
+(longitude DECIMAL,
+ latitude DECIMAL, 
+ uid CHAR(10),
+ post_no INTEGER,
+ image_URL VARCHAR(100)
  PRIMARY KEY(uid, post_no),
  FOREIGN KEY(uid, post_no) REFERENCES Dep_posts
 	ON DELETE CASCADE)
@@ -49,11 +49,11 @@ CREATE TABLE Personal_mood
 -- non-overlapping condition between Personal_mood and Group_post
 
 CREATE TABLE Dep_comments         --weak entity relationship
-(comment_no : INTEGER,
- uid_comment : CHAR(10),
- uid_post : CHAR(10), --NOT NULL
- post_no : INTEGER, -- NOT NULL
- text : VARCHAR(200),
+(comment_no INTEGER,
+ uid_comment CHAR(10),
+ uid_post CHAR(10) NOT NULL,
+ post_no INTEGER NOT NULL,
+ text VARCHAR(200),
  PRIMARY KEY(uid_comment, comment_no),
  FOREIGN KEY(uid_comment) REFERENCES Users
  	ON DELETE CASCADE
@@ -64,9 +64,9 @@ CREATE TABLE Dep_comments         --weak entity relationship
 --  participation of Group to the User (If no users stays in a group, that group will be deleted automatically)
 
 CREATE TABLE User_in_group
-(uid : INTEGER
- group_id : CHAR(10),
- level : INTEGER,
+(uid INTEGER
+ group_id CHAR(10),
+ level INTEGER,
  PRIMARY KEY(uid, group_id),
  FOREIGN KEY(uid) REFERENCES Users
  	ON DELETE CASCADE
@@ -75,8 +75,8 @@ CREATE TABLE User_in_group
 
 
 CREATE TABLE Follow
-(uid_following : CHAR(10),
- uid_followed : CHAR(10),
+(uid_following CHAR(10),
+ uid_followed CHAR(10),
  PRIMARY KEY (uid_following, uid_followed),
  FOREIGN KEY(uid_following) REFERENCES Users
 	ON DELETE CASCADE
@@ -95,11 +95,11 @@ CREATE TABLE comments_to_comments
 )
 
 CREATE TABLE Responses_to_post
-(uid_post : CHAR(10),
- post_no : INTEGER,
- uid_like : CHAR(10),
- mood : INTEGER,
- time : DATETIME
+(uid_post CHAR(10),
+ post_no INTEGER,
+ uid_like CHAR(10),
+ mood INTEGER,
+ time DATETIME
  PRIMARY KEY(uid_post, post_no, uid_like),
  FOREIGN KEY(uid_post, post_no) REFERENCES Dep_posts
 	ON DELETE CASCADE
@@ -108,11 +108,11 @@ CREATE TABLE Responses_to_post
 
 
 CREATE TABLE Responses_to_comment
-(uid_comment : CHAR(10),
- comment_no : INTEGER,
- uid_like : CHAR(10),
- mood : INTEGER,
- time : DATETIME
+(uid_comment CHAR(10),
+ comment_no INTEGER,
+ uid_like CHAR(10),
+ mood INTEGER,
+ time DATETIME
  PRIMARY KEY(uid_comment, comment_no, uid_like),
  FOREIGN KEY(uid_comment, comment_no) REFERENCES Dep_comments
 	ON DELETE CASCADE
