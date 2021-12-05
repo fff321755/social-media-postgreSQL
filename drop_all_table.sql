8534e874aa413f2e57b8807e8c76471291d88bef
fff321755 <fff321755@gmail.com>
2021-12-05 15:35:37 -0500

    fix drop

--- drop_all_table.sql
+++ drop_all_table.sql
@@ -1,3 +1,11 @@
+drop trigger empty_group on groups;
+drop trigger delete_empty_group on user_in_group;
+
+drop function empty_group;
+drop function delete_empty_group;
+
+drop table mood_in_location ;
+drop table location ;
 drop table responses_to_post ;
 drop table responses_to_comment ;
 drop table comments_to_comments ;
@@ -9,3 +17,6 @@ drop table dep_posts ;
 drop table user_in_group ;
 drop table groups ;
 drop table users ;
+
+
+drop type location_type;
--- trigger.sql
+++ trigger.sql
@@ -5,7 +5,7 @@ ADD CONSTRAINT user_in_group_group_id_fkey
    FOREIGN KEY (group_id) REFERENCES Groups ON DELETE CASCADE ON UPDATE CASCADE
    DEFERRABLE INITIALLY DEFERRED;
 
-CREATE FUNCTION empty_group() RETURNS TRIGGER AS $empty_group$
+CREATE FUNCTION empty_group() RETURNS TRIGGER AS $body$
     BEGIN
         IF (NEW.group_id NOT IN (SELECT group_id FROM User_in_group)) THEN
             DELETE FROM Groups G 
@@ -13,7 +13,7 @@ CREATE FUNCTION empty_group() RETURNS TRIGGER AS $empty_group$
         END IF;
         RETURN NULL;
     END;
-$empty_group$ LANGUAGE plpgsql;
+$body$ LANGUAGE plpgsql;
 
 CREATE CONSTRAINT TRIGGER empty_group
 AFTER INSERT ON Groups
@@ -22,7 +22,7 @@ DEFERRABLE INITIALLY DEFERRED
 
 
 -- trigger 2
-CREATE FUNCTION delete_empty_group() RETURNS trigger AS $body$
+CREATE FUNCTION delete_empty_group() RETURNS TRIGGER AS $body$
 
     BEGIN
         IF (SELECT COUNT(*) FROM User_in_group ug WHERE group_id = old.group_id)= 0 THEN
